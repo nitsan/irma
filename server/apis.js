b463b5ab80b8f1c4ac2e4f430475aa4a0497c938
nitsan <mintaz18>
2016-06-30 21:32:45 +0300

    bump version and add /version

--- server/apis.js
+++ server/apis.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var pjson = require('../package.json');
 
-// var taskModel = require('./taskModel.js')
+router.get('/version', function (req, res) {
+    res.send({
+        version: pjson.version,
+        description: pjson.description,
+        author: pjson.author
+    });
+});
 
-router.get('/', function(req, res){
-        res.send("Root!");
-    });
-router.get('/test1', function(req, res){
-        res.send({test1: "OK!"});
-    });
+router.get('/test1', function (req, res) {
+    res.send({test1: "OK!"});
+});
 
 
 module.exports = router;
